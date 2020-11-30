15954bd7d70d5c2f50ad367a5c0acc0f2278405d
bvidalneofox <v_andres01@hotmail.com>
2020-11-30 20:35:53 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,115 @@
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
+
+const registroPorPagina = 30;
+let totalPaginas;
+let iterador;
+let paginaActual = 1;
+
+window.onload = () => {
+    formulario.addEventListener('submit', validarFomulario);
+}
+
+function validarFomulario(e) {
+    e.preventDefault();
+    
+    const terminoBusqueda = document.querySelector('#termino').value;
+
+    if(terminoBusqueda === ''){
+        mostrarAlerta('Ingresa un termino de busqueda');
+        return;
+    }
+
+    buscarImagenes();
+}
+
+function mostrarAlerta(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+    alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block sm:inline">${mensaje}</span>
+    `;
+    formulario.appendChild(alerta);
+}
+
+function buscarImagenes() {
+    const termino = document.querySelector('#termino').value;
+
+    const key = '19334814-bc9b05d1ed3cf2e0cc1a1d765';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&image_type=photo&per_page=${registroPorPagina}&page=${paginaActual}`;
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => {
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        });
+}
+
+// Generador para el paginador
+function *crearPaginador(total) {
+    for(let i = 1; i <= total; i++){
+        yield i;
+    }
+}
+
+function  calcularPaginas(total) {
+    return parseInt(Math.ceil(total / registroPorPagina));
+}
+
+function mostrarImagenes(imagenes) {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    // Iterar las imagenes
+    imagenes.forEach(imagen => {
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}">
+                    <div class="p-4">
+                        <p class="font-bold">${likes} <span class="font-light">Me gusta</span></p>
+                        <p class="font-bold">${views} <span class="font-light">Veces Vista</span></p>
+
+                        <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1" href="${largeImageURL}" target="_blank">Ver Imagen</a>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    while (paginacionDiv.firstChild) {
+        paginacionDiv.removeChild(paginacionDiv.firstChild);
+    }
+
+    imprimirPaginador();
+}
+
+function imprimirPaginador() {
+    iterador = crearPaginador(totalPaginas);
+
+    while (true) {
+        const { value, done } = iterador.next();
+        if(done) return;
+
+        // Caso contrario genera un boton por cada elemento
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-10', 'uppercase', 'rounded');
+
+        boton.onclick = () => {
+            paginaActual = value;
+            buscarImagenes();
+        }
+
+        paginacionDiv.appendChild(boton);
+    }
+}
